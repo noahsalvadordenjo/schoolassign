3fbce2263a7ea402ba319a345e80b2217c28c319
MassiveStory21! <noahvandenbergh@hotmail.nl>
2020-09-17 14:31:18 +0200

    second

--- ValueConverter.java
+++ ValueConverter.java
@@ -11,12 +11,20 @@ public class ValueConverter {
         double Hg = devidedBy * 2.54;
         return Hg * 1.33322;
     }
+    public static double temperature(short rawValue)
+    {
+        double temperature;
+        temperature = (((double)rawValue / 10) - 32) / 1.8;
+        System.out.println("Temperature: " + temperature);
+        return temperature;
+    }
     public static double humidity(short rawValue) {
         return (double) rawValue;
     }
     public static double windSpeed(short rawValue) {
         return (double) rawValue * 1.61;
     }
+    
     //double 0 = north
     //double 1 = east
     //double 2 = south
@@ -28,7 +36,25 @@ public class ValueConverter {
     public static double rainRate(short rawValue) {
         return (double) rawValue / 100.00;
     }
+    public static double uvIndex(short rawValue)
+    {
+        double uvIndex;
+        uvIndex = ((double)rawValue / 10);
+        System.out.println("uvIndex: " + uvIndex);
+        return uvIndex;
+    }
     public static double batteryLevel(short rawValue) {
         return (double) ((rawValue * 300.00) / 512.00) / 100.00;
+    }
+ 
+    public static String sunRiseSet(short rawValue)
+    {
+        String time;
+        time = String.valueOf(rawValue);
+        String time1 = time.substring(0, (time.length()/2));
+        String time2 = time.substring((time.length()/2));
+
+        System.out.println("Time: " + time1 + ":" + time2);
+        return time;
     }
 }
